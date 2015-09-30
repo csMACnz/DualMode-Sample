1099ad1e2c087ddc6cafcc7cbfbe89e1b7d0b0a4
Mark Clearwater <csmacnz@csmac.co.nz>
2015-10-01 10:08:05 +1300

    Updated to launch in either console or gui mode.

--- src/DualMode-Sample/Program.cs
+++ src/DualMode-Sample/Program.cs
@@ -1,28 +1,82 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace DualMode_Sample
 {
     public class Program
     {
-        [System.STAThread]
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        //const int SW_SHOW = 5;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [STAThread]
         public static void Main(string[] argv)
         {
+            //http://stackoverflow.com/questions/493536/can-one-executable-be-both-a-console-and-gui-application
             if (argv.Length == 0)
             {
-                App.Main();
+                LaunchAsGui();
             }
             else
             {
                 var args = new MainArgs(argv, help: true, version: "1.0.0", exit: true);
                 if (args.OptGui)
                 {
-                    App.Main();
+                    LaunchAsGui();
                 }
                 else if (args.OptCli)
                 {
-                    Console.WriteLine("I'm a console App");
+                    RunInConsole(args);
                 }
             }
+        }
+
+        private static void LaunchAsGui()
+        {
+            if (IsInConsole())
+            {
+                //relaunch as separate process
+                var currentProcess = Process.GetCurrentProcess().MainModule.FileName;
+                Process.Start(currentProcess);
+            }
+            else
+            {
+                var handle = GetConsoleWindow();
+
+                // Hide
+                ShowWindow(handle, SW_HIDE);
+
+                App.Main();
+            }
+        }
+
+        private static bool IsInConsole()
+        {
+            // Get uppermost window process
+            IntPtr ptr = GetForegroundWindow();
+            int u;
+            GetWindowThreadProcessId(ptr, out u);
+            Process process = Process.GetProcessById(u);
+
+            // Check if it is console?
+            return process.ProcessName == "cmd";
+        }
+
+        private static void RunInConsole(MainArgs args)
+        {
+            Console.WriteLine("I'm a console App");
         }
     }
 }
